bcdb1879018e2a9cfaea2f05b5cf9226bf652b95
airat05041999 <77199623+airat05041999@users.noreply.github.com>
2022-04-20 16:26:32 +0300

    сдвиг
    сдвиг

--- sourse/Csos.cpp
+++ sourse/Csos.cpp
@@ -4,7 +4,7 @@
 #define  NUMBER_IS_2_POW_K(x)   ((!((x)&((x)-1)))&&((x)>1))  // x is pow(2, k), k=1,2, ...
 #define  FT_DIRECT        -1    // Direct transform.
 #define  FT_INVERSE        1    // Inverse transform.
-#define number 8
+#define number 64
 
 //________________________________________________________________________________________________ 
 //______________________________________________________________________________________________ 
@@ -23,7 +23,7 @@
 // int Ft_Flag [in] - Ft_Flag = FT_ERR_DIRECT (т.е. -1) - Прямое БПФ (от сигнала к спектру) 
 // Ft_Flag = FT_ERR_INVERSE (т.е. 1) - Обратное БПФ (от спектра к сигналу) 
 // 
-// ВОЗВРАЩАЕМОЕ ЗНАЧЕНИЕ: false при ошибке параметра 
+// ВОЗВРАЩАЕМОЕ ЗНАЧЕНИЕ: false при ошибке параметра s
 //_________________________________________________________________________________________ 
 // 
 // ПРИМЕЧАНИЕ: В этом алгоритме N и LogN могут быть только: 
@@ -32,56 +32,79 @@
 //________________________________________________________________________________________________ 
 //______________________________________________________________________________________________
 
-bool shift(float* Rdat, float* Idat, int N, int M)
-{
-    if ((Rdat == NULL) || (Idat == NULL))                  return false;
-    if ((N > 16384) || (N < 1))                            return false;
-    if (!NUMBER_IS_2_POW_K(N))                             return false;
-    int i, j;
+void invert(float* Rdat, float* Idat, int N) {
+    int i = 0;
+    int j = N - 1;
     float memr, memi;
-    memr = Rdat[i];
-    memi = Idat[i];
-        for (i = 0; i < M; i++)
-        {
-            Rdata[i] = Rdat[N - M + i];
-            Idata[i] = Idat[N - M + i];
-            Rdata[N - M + i] = Rdat[N - M + i - 1];
-            Idata[N - M + i] = Idat[N - M + i - 1];
-        }
-    return true;
+    while (i < j) {
+        memr = Rdat[i];
+        memi = Idat[i];
+        Rdat[i] = Rdat[j];
+        Idat[i] = Idat[j];
+        Rdat[j] = memr;
+        Idat[j] = memi;
+        ++i; 
+        --j;
+    }
+}
+
+void shiftk(float* Rdat, float* Idat, int N, int k) {
+    k %= N;
+    if (k < 0)  // Сдвиг влево на k эквивалентен
+        k += N; // сдвигу вправо на n-k
+    invert(Rdat, Idat, N - k);   // Инвертируем начало массива длины n-k
+    invert(Rdat + N - k, Idat + N - k, k); // Инвертируем конец массива длины k
+    invert(Rdat, Idat, N);     // Инвертируем весь массив
 }
 
 bool  FFT(float *Rdat, float *Idat, int N, int LogN, int Ft_Flag)
 {
-  // parameters error check:
-  if((Rdat == NULL) || (Idat == NULL))                  return false;
-  if((N > 16384) || (N < 1))                            return false;
-  if(!NUMBER_IS_2_POW_K(N))                             return false;
-  if((LogN < 2) || (LogN > 14))                         return false;
-  if((Ft_Flag != FT_DIRECT) && (Ft_Flag != FT_INVERSE)) return false;
+    // parameters error check:
+    if((Rdat == NULL) || (Idat == NULL))  
+    {
+        return false;
+    }
+    if((N > 16384) || (N < 1))                            
+    {
+        return false;
+    }
+    if(!NUMBER_IS_2_POW_K(N))                             
+    {
+        return false;
+    }
+    if((LogN < 2) || (LogN > 14))                         
+    {
+        return false;
+    }
+    if((Ft_Flag != FT_DIRECT) && (Ft_Flag != FT_INVERSE)) 
+    {
+        return false;
+    }
 
-  register int  i, j, n, k, io, ie, in, nn;
-  float         ru, iu, rtp, itp, rtq, itq, rw, iw, sr;
+    register int  i, j, n, k, io, ie, in, nn;
+    float         ru, iu, rtp, itp, rtq, itq, rw, iw, sr;
   
-  static const float Rcoef[14] =
-  {  -1.0000000000000000F,  0.0000000000000000F,  0.7071067811865475F,
-      0.9238795325112867F,  0.9807852804032304F,  0.9951847266721969F,
-      0.9987954562051724F,  0.9996988186962042F,  0.9999247018391445F,
-      0.9999811752826011F,  0.9999952938095761F,  0.9999988234517018F,
-      0.9999997058628822F,  0.9999999264657178F
-  };
-  static const float Icoef[14] =
-  {   0.0000000000000000F, -1.0000000000000000F, -0.7071067811865474F,
-     -0.3826834323650897F, -0.1950903220161282F, -0.0980171403295606F,
-     -0.0490676743274180F, -0.0245412285229122F, -0.0122715382857199F,
-     -0.0061358846491544F, -0.0030679567629659F, -0.0015339801862847F,
-     -0.0007669903187427F, -0.0003834951875714F
-  };
+    static const float Rcoef[14] =
+    {  
+        -1.0000000000000000F,  0.0000000000000000F,  0.7071067811865475F,
+        0.9238795325112867F,  0.9807852804032304F,  0.9951847266721969F,
+        0.9987954562051724F,  0.9996988186962042F,  0.9999247018391445F,
+        0.9999811752826011F,  0.9999952938095761F,  0.9999988234517018F,
+        0.9999997058628822F,  0.9999999264657178F
+    };
+    static const float Icoef[14] =
+    {  
+        0.0000000000000000F, -1.0000000000000000F, -0.7071067811865474F,
+        -0.3826834323650897F, -0.1950903220161282F, -0.0980171403295606F,
+        -0.0490676743274180F, -0.0245412285229122F, -0.0122715382857199F,
+        -0.0061358846491544F, -0.0030679567629659F, -0.0015339801862847F,
+        -0.0007669903187427F, -0.0003834951875714F
+    };
   
-  nn = N >> 1;
-  ie = N;
-  for(n=1; n<=LogN; n++)
-  {
+    nn = N >> 1;
+    ie = N;
+    for(n=1; n<=LogN; n++)
+    {
     rw = Rcoef[LogN - n];
     iw = Icoef[LogN - n];
     if(Ft_Flag == FT_INVERSE) iw = -iw;
@@ -90,54 +113,54 @@ bool  FFT(float *Rdat, float *Idat, int N, int LogN, int Ft_Flag)
     iu = 0.0F;
     for(j=0; j<in; j++)
     {
-      for(i=j; i<N; i+=ie)
-      {
-        io       = i + in;
-        rtp      = Rdat[i]  + Rdat[io];
-        itp      = Idat[i]  + Idat[io];
-        rtq      = Rdat[i]  - Rdat[io];
-        itq      = Idat[i]  - Idat[io];
-        Rdat[io] = rtq * ru - itq * iu;
-        Idat[io] = itq * ru + rtq * iu;
-        Rdat[i]  = rtp;
-        Idat[i]  = itp;
-      }
-      sr = ru;
-      ru = ru * rw - iu * iw;
-      iu = iu * rw + sr * iw;
+        for(i=j; i<N; i+=ie)
+        {
+            io       = i + in;
+            rtp      = Rdat[i]  + Rdat[io];
+            itp      = Idat[i]  + Idat[io];
+            rtq      = Rdat[i]  - Rdat[io];
+            itq      = Idat[i]  - Idat[io];
+            Rdat[io] = rtq * ru - itq * iu;
+            Idat[io] = itq * ru + rtq * iu;
+            Rdat[i]  = rtp;
+            Idat[i]  = itp;
+        }
+        sr = ru;
+        ru = ru * rw - iu * iw;
+        iu = iu * rw + sr * iw;
     }
     ie >>= 1;
-  }
+    }
 
-  for(j=i=1; i<N; i++)
-  {
-    if(i < j)
+    for(j=i=1; i<N; i++)
     {
-      io       = i - 1;
-      in       = j - 1;
-      rtp      = Rdat[in];
-      itp      = Idat[in];
-      Rdat[in] = Rdat[io];
-      Idat[in] = Idat[io];
-      Rdat[io] = rtp;
-      Idat[io] = itp;
-    }
-    k = nn;
-    while(k < j)
+        if(i < j)
     {
-      j   = j - k;
-      k >>= 1;
+        io       = i - 1;
+        in       = j - 1;
+        rtp      = Rdat[in];
+        itp      = Idat[in];
+        Rdat[in] = Rdat[io];
+        Idat[in] = Idat[io];
+        Rdat[io] = rtp;
+        Idat[io] = itp;
     }
-    j = j + k;
-  }
-  if(Ft_Flag == FT_DIRECT) return true;
-  rw = 1.0F / N;
-  for(i=0; i<N; i++)
-  {
-    Rdat[i] *= rw;
-    Idat[i] *= rw;
-  }
-  return true;
+        k = nn;
+        while(k < j)
+    {
+        j   = j - k;
+        k >>= 1;
+    }
+    
+    }
+        if(Ft_Flag == FT_DIRECT) return true;
+        rw = 1.0F / N;
+        for(i=0; i<N; i++)
+    {
+        Rdat[i] *= rw;
+        Idat[i] *= rw;
+    }
+    return true;
 }
 
 
@@ -149,7 +172,7 @@ void main()
     static float Re[number];
     static float Im[number];
     float  p = 2 * 3.141592653589 / number; // будет number отсчетов на период
-    int i, log;
+    int i, log, k = 2;
     log = log2(number);
     // формируем сигнал
     for(i=0; i< number; i++)
@@ -167,17 +190,19 @@ void main()
 
     FFT(Re, Im, number, log, FT_DIRECT); // вычисляем прямое БПФ
 
+    shiftk(Re, Im, number, k);
+
     // выводим действительную и мнимую части спектра и спектр мощности
     FILE *a = fopen("spectrum.txt", "w");
     for(i=0; i< number; i++)
     {
-        fprintf(a, "%10.6f  %10.6f  %10.6f\n", Re[i], Im[i], Re[i] * Re[i] + Im[i] * Im[i]);
+    fprintf(a, "%10.6f  %10.6f  %10.6f\n", Re[i], Im[i], Re[i]*Re[i]+Im[i]*Im[i]);
     }
     fclose(a);
 
     FFT(Re, Im, number, log, FT_INVERSE); // вычисляем обратное БПФ
 
-    // выводим действительную и мнимую части спектра и спектр мощности
+// выводим действительную и мнимую части спектра и спектр мощности
     FILE* b = fopen("vostsignal.txt", "w");
     for (i = 0; i < number; i++)
     {
